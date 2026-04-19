acac0f32502ae4e1a8785d9a73c4649cc7268be9
Guilherme Torres <150628871+guilherme-torresz@users.noreply.github.com>
2026-04-18 21:43:06 -0300

    Update calculadora-idade-ano-meses-semanas.c
    Calculadora de idade em anos, meses e semanas,

--- calculadora-idade-ano-meses-semanas.c
+++ calculadora-idade-ano-meses-semanas.c
@@ -1,21 +1,21 @@
-//Programa que lê o ano em que o usuário nasceu e calcula sua idade em anos, meses e semanas, tendo como ano atual 2026.
-
 #include <stdio.h>
 
 int main()
 
 {
-    float data_nascimento,idade_anos,idade_meses,idade_semanas;
+    float data_nascimento,ano_atual,idade_anos,idade_meses,idade_semanas;
 
     printf("\nDigite o seu ano de nascimento: ");
     scanf("%f",&data_nascimento);
 
+    printf("\nDigite o ano atual: ");
+    scanf("%f",&ano_atual);
 
-    idade_anos = 2026 - data_nascimento;//calcula a idade em anos
+    idade_anos = ano_atual - data_nascimento;
 
     printf("\nA sua idade em anos é %.f anos\n",idade_anos);
 
-    idade_meses = idade_anos * 12;//calcula a idade em meses
+    idade_meses = idade_anos * 12;
 
     printf("\nA sua idade em meses é %.f meses\n\n",idade_meses);
 
